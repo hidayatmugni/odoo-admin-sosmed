5f4ec207c490beef29cecb3ce4a2ac64eba96d26
Mugni Hidayat <166070300+hidayatmugni@users.noreply.github.com>
2025-07-31 11:57:00 +0700

    Create __manifets__.py

---
+++ __manifets__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Programmer : Mugni
+
+{
+    "name": "GM Sosmed Admin",
+    "summary": "Sosmed Admin",
+    "description": """
+Admin Work Statistic and Report
+    """,
+    "author": "Galeri Medika",
+    "website": "https://www.yourcompany.com",
+    "category": "Sales",
+    "version": "1.2",
+    "depends": ["base", "web"],
+    "data": [
+        # Security
+        "security/admin_work_security.xml",
+        "security/ir.model.access.csv",
+        # report
+        "report/admin_work_templates.xml",
+        "report/admin_work_reports.xml",
+        # views
+        "views/admin_work_views.xml",
+        # chart action
+        "views/chart_action.xml",
+        # views content
+        "views/content_report_stream_tiktok_views.xml",
+        "views/content_report_stream_shopee_views.xml",
+        "views/content_report_video_views.xml",
+        "views/product_knowledge_views.xml",
+        "views/article_video_views.xml",
+        # Chart views
+        "views/chart_tiktok_template.xml",
+        "views/chart_shopee_template.xml",
+        "views/chart_video_template.xml",
+        # menu
+        "views/admin_work_menus.xml",
+    ],
+    "application": True,
+    "installable": True,
+    "license": "OPL-1",
+}
